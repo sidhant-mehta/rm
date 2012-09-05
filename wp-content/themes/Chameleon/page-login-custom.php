51c2671c17fce6e53f524884395b9ca5b60e1021
Sidhant Mehta <sidhant@sidhant-Ferrari-One.(none)>
2012-09-05 13:53:01 +0100

    first commit. login needs work

---
+++ wp-content/themes/Chameleon/page-login-custom.php
@@ -0,0 +1,58 @@
+<?php 
+/*
+Template Name: Login Page (Custom)
+*/
+?>
+<?php 
+	$et_ptemplate_settings = array();
+	$et_ptemplate_settings = maybe_unserialize( get_post_meta($post->ID,'et_ptemplate_settings',true) );
+	
+	$fullwidth = isset( $et_ptemplate_settings['et_fullwidthpage'] ) ? (bool) $et_ptemplate_settings['et_fullwidthpage'] : false;
+?>
+
+<?php get_header(); ?>
+
+<?php get_template_part('includes/breadcrumbs'); ?>
+<?php get_template_part('includes/top_info'); ?>
+
+<div id="content" class="clearfix<?php if($fullwidth) echo(' fullwidth');?>">
+	<div id="left-area">
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<div class="entry post clearfix">							
+			<?php if (get_option('chameleon_page_thumbnails') == 'on') { ?>
+				<?php 
+					$thumb = '';
+					$width = 186;
+					$height = 186;
+					$classtext = 'post-thumb';
+					$titletext = get_the_title();
+					$thumbnail = get_thumbnail($width,$height,$classtext,$titletext,$titletext,false,'Entry');
+					$thumb = $thumbnail["thumb"];
+				?>
+				
+				<?php if($thumb <> '') { ?>
+					<div class="post-thumbnail">
+						<?php print_thumbnail($thumb, $thumbnail["use_timthumb"], $titletext, $width, $height, $classtext); ?>
+						<span class="post-overlay"></span>
+					</div> 	<!-- end .post-thumbnail -->
+				<?php } ?>
+			<?php } ?>
+			
+			<?php the_content(); ?>
+			<?php wp_link_pages(array('before' => '<p><strong>'.esc_html__('Pages','Chameleon').':</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+			
+			<div id="login">
+			  <?php theme_my_login( ); ?>
+			</div>
+			
+			<div class="clear"></div>
+			
+			<?php edit_post_link(esc_html__('Edit this page','Chameleon')); ?>			
+		</div> <!-- end .entry -->
+	<?php endwhile; endif; ?>
+	</div> 	<!-- end #left-area -->
+
+	<?php if (!$fullwidth) get_sidebar(); ?>
+</div> <!-- end #content -->
+		
+<?php get_footer(); ?>
