3f94a2426cf5b828df642a922c65d7e9030dd6aa
Sidhant Mehta <sidhant_mehta@yahoo.com>
2012-09-13 12:56:23 +0100

    changed ajax file so it redirects if someone visits it directly

--- wp-content/themes/Chameleon/ajax.php
+++ wp-content/themes/Chameleon/ajax.php
@@ -11,3 +11,18 @@ $_SESSION['emailTypeValue'] = $_POST['emailType']; //store type of application -
 $_SESSION['emailTypeNameValue'] = $_POST['emailTypeName']; //store name of applicatoin. - Name of Mentor or Name of Project
 
 ?>
+
+<html>
+<head>
+<title> You are being redirected</title>
+</head>
+<body>
+    You are being redirected
+      <script type="text/javascript">
+  
+  window.onload = function(){
+	window.location = "<?php echo get_bloginfo('url'); ?>";
+  }
+  </script>
+</body>
+</html>
