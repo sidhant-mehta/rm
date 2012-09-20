7585fbaa675dd154829f4646235d2316b29d67e9
Sidhant Mehta <sidhant_mehta@yahoo.com>
2012-09-20 15:32:53 +0100

    quick fix for homepage. and footer. Should not have done it on this branch!

--- wp-content/themes/Chameleon/footer.php
+++ wp-content/themes/Chameleon/footer.php
@@ -4,7 +4,7 @@
 					<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer') ) : ?>
 					<?php endif; ?>
 				</div> <!-- end #footer-widgets -->
-				<p id="copyright"><?php esc_html_e('Designed by ','Chameleon'); ?> <a href="http://www.elegantthemes.com" title="Premium WordPress Themes">Elegant WordPress Themes</a> | <?php esc_html_e('Powered by ','Chameleon'); ?> <a href="http://www.wordpress.org">WordPress</a></p>
+				<p id="copyright" style="color: rgb(146, 146, 146);" ><?php esc_html_e('Developed by ','Chameleon'); ?>Sidhant Mehta</p>
 			</div> <!-- end #footer-content -->
 		</div> <!-- end #footer -->
 	</div> <!-- end #container -->
